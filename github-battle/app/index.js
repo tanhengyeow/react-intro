08983b6d7aff18fc64cfb280dbf2886b9d4ea9cd
root <tanhengyeow@gmail.com>
2017-06-29 02:12:09 -0700

    React fundamentals template

---
+++ github-battle/app/index.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom'); //render react component with the dom
+
+require('./index.css') //all css in index.css included inside the application when webpack bundles up everything
+
